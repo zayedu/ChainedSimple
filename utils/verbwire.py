94169c365bcedcfd0f87747e67ddb2ef7455ed7c
zayedu <umerzayed1@gmail.com>
2025-01-11 12:29:41 -0500

    feat: use verbwire to mint an nft

--- utils/verbwire.py
+++ utils/verbwire.py
@@ -16,4 +16,29 @@ VERBWIRE_BASE_URL = "https://api.verbwire.com/v1"
 
 MINT_FROM_METADATA_ENDPOINT = f"{VERBWIRE_BASE_URL}/nft/mint/quickMintFromMetadata"
 
-pprint(MINT_FROM_METADATA_ENDPOINT)
+def mint_nft_from_metadata_url(metadata_url, wallet_address, chain="sepolia"):
+    """
+    use quick mint from metadata url endpoint to mint an NFT and associate with users wallet address
+    """
+    quickMintFromMetadataUrl_endpoint = f"{VERBWIRE_BASE_URL}/nft/mint/quickMintFromMetadataUrl"
+
+    form_data = {
+        "metadataUrl": (None, metadata_url),
+        "recipientAddress": (None, wallet_address),
+        "allowPlatformToOperateToken": (None, "true"),
+        "chain": (None, chain)
+    }
+
+    headers = {
+        "X-API-Key": VERBWIRE_API_KEY,
+        "accept": "application/json"
+    }
+
+    response = requests.post(quickMintFromMetadataUrl_endpoint, files=form_data, headers=headers)
+    try:
+        return response.json()
+    except Exception:
+        return {
+            "error": "Failed to parse JSON from Verbwire response.",
+            "raw": response.text
+        }
