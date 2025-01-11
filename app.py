29d777da4a3d91c5787dda985e74deba8a392e8a
zayedu <umerzayed1@gmail.com>
2025-01-11 12:16:12 -0500

    feat: setup verbwire api key

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+import os
+from dotenv import load_dotenv
+from pprint import pprint
 app = Flask(__name__)
 
 
+load_dotenv()
+VERBWIRE_API_KEY = os.getenv("VERBWIRE_API_KEY")
+pprint(VERBWIRE_API_KEY)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
