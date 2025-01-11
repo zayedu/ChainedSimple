eba92577c1a22cd352ec31de26bb4ab99c78590d
zayedu <umerzayed1@gmail.com>
2025-01-11 12:24:42 -0500

    feat: getting started with minting an nft from verbwire

--- utils/verbwire.py
+++ utils/verbwire.py
@@ -8,4 +8,12 @@ from pprint import pprint
 load_dotenv()
 VERBWIRE_API_KEY = os.getenv("VERBWIRE_API_KEY")
 
+pprint(VERBWIRE_API_KEY)
+
 VERBWIRE_BASE_URL = "https://api.verbwire.com/v1"
+
+#Endpoints (just minting an nft for rn)
+
+MINT_FROM_METADATA_ENDPOINT = f"{VERBWIRE_BASE_URL}/nft/mint/quickMintFromMetadata"
+
+pprint(MINT_FROM_METADATA_ENDPOINT)
